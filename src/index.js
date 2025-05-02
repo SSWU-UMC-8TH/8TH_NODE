cba89d753c1fb861d00c117b21c18ea2f11523f7
rimit-rim <sunsor0105@naver.com>
2025-05-02 23:57:49 +0900

    3. 가게에 미션 추가하기 API

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import express from "express";
 import { handleUserSignUp } from "./controllers/user.controller.js";
 import { handleCreateStore } from "./controllers/store.controller.js";
 import { handleCreateReview } from "./controllers/review.controller.js";
+import { handleCreateMission } from "./controllers/mission.controller.js";
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ app.get("/", (req, res) => {
 app.post("/api/v1/users/signup", handleUserSignUp);
 app.post("/api/stores", handleCreateStore);
 app.post("/api/stores/:storeId/reviews", handleCreateReview);
+app.post("/api/stores/:storeId/missions", handleCreateMission);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
