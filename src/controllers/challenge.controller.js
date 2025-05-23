fe55647765621ea6a08b738013188f0b7f955a96
rimit-rim <sunsor0105@naver.com>
2025-05-23 17:05:03 +0900

    Week8 미션

--- src/controllers/challenge.controller.js
+++ src/controllers/challenge.controller.js
@@ -17,9 +17,112 @@ export const handleCreateChallenge = async (req, res) => {
 // 특정 사용자의 미션 목록을 조회하는 핸들러
 export const handleListUserChallenges = async (req, res) => {
     try {
-        const userId = parseInt(req.params.userId);
-        const challenges = await listUserChallenges(userId);
-        res.status(StatusCodes.OK).success(challenges);
+        /*
+#swagger.summary = '사용자가 진행 중인 미션 목록 리뷰 목록 API';
+#swagger.responses[200] = {
+  description: "사용자 미션 목록 조회 성공 응답",
+  content: {
+    "application/json": {
+      schema: {
+        type: "object",
+        properties: {
+          resultType: { type: "string", example: "SUCCESS" },
+          error: { type: "object", nullable: true, example: null },
+          success: {
+            type: "object",
+            properties: {
+              data: {
+                type: "array",
+                items: {
+                  type: "object",
+                  properties: {
+                    id: { type: "number" },
+                    createdAt: {
+                      type: "string",
+                      format: "date-time",
+                      example: "yyyy-mm-ddT00:00:00.000Z"
+                    },
+                    mission: {
+                      type: "object",
+                      properties: {
+                        id: { type: "number" },
+                        reward: { type: "string" },
+                        missionSpec: { type: "string" },
+                        deadline: {
+                          type: "string",
+                          format: "date-time",
+                          example: "yyyy-mm-ddT00:00:00.000Z"
+                        },
+                        store: {
+                          type: "object",
+                          properties: {
+                            id: { type: "number" },
+                            name: { type: "string" }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              },
+              pagination: {
+                type: "object",
+                properties: {
+                  cursor: { type: "number", nullable: true }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
+#swagger.responses[404] = {
+      description: "존재하지 않는 사용자의 미션 조회 실패 응답",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              resultType: { type: "string", example: "FAIL" },
+              error: {
+                type: "object",
+                properties: {
+                  errorCode: { type: "string", example: "U002" },
+                  reason: { type: "string" },
+                  data: { type: "object" }
+                }
+              },
+              success: { type: "object", nullable: true, example: null }
+            }
+          }
+        }
+      }
+    };
+    #swagger.responses[400] = {
+      description: "유효하지 않는 userID의 미션 조회 실패 응답",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              resultType: { type: "string", example: "FAIL" },
+              error: {
+                type: "object",
+                properties: {
+                  errorCode: { type: "string", example: "U003" },
+                  reason: { type: "string" },
+                  data: { type: "object" }
+                }
+              },
+              success: { type: "object", nullable: true, example: null }
+            }
+          }
+        }
+      }
+    };
+*/
     } catch (err) {
         res.status(StatusCodes.BAD_REQUEST).json({ message: err.message });
     }
@@ -28,8 +131,81 @@ export const handleListUserChallenges = async (req, res) => {
 // 사용자가 도전 중인 미션을 완료 처리하는 핸들러
 export const handleCompleteChallenge = async (req, res, next) => {
     try {
-        const challengeId = parseInt(req.params.challengeId);
-        const id = await completeChallenge(challengeId);
+        /*
+#swagger.summary = '미션 완료 API';
+#swagger.responses[200] = {
+  description: "사용자 미션 완료 처리 성공 응답",
+  content: {
+    "application/json": {
+      schema: {
+        type: "object",
+        properties: {
+          resultType: { type: "string", example: "SUCCESS" },
+          error: { type: "object", nullable: true, example: null },
+          success: {
+            type: "object",
+            properties: {
+              data: {
+                type: "array",
+                items: {
+                  type: "object",
+                  properties: {
+                    success: { type: "object", properties: { id: { type: "number" }, message: { type: "string" } } },
+                  }
+                }
+              },
+            }
+          }
+        }
+      }
+    }
+  }
+};
+#swagger.responses[404] = {
+      description: "존재하지 않는 미션일 때 응답",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              resultType: { type: "string", example: "FAIL" },
+              error: {
+                type: "object",
+                properties: {
+                  errorCode: { type: "string", example: "U007" },
+                  reason: { type: "string" },
+                  data: { type: "object" }
+                }
+              },
+              success: { type: "object", nullable: true, example: null }
+            }
+          }
+        }
+      }
+    };
+    #swagger.responses[400] = {
+      description: "이미 완료된 미션일 때 실패 응답",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              resultType: { type: "string", example: "FAIL" },
+              error: {
+                type: "object",
+                properties: {
+                  errorCode: { type: "string", example: "U006" },
+                  reason: { type: "string" },
+                  data: { type: "object" }
+                }
+              },
+              success: { type: "object", nullable: true, example: null }
+            }
+          }
+        }
+      }
+    };
+*/
         res.status(StatusCodes.OK).success({ id, message: "미션이 완료되었습니다." });
     } catch (err) {
         next(err);
--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -5,69 +5,140 @@ import { InvalidUserIdFormatError } from "../errors.js";
 
 // 특정 가게에 대한 리뷰를 작성하는 핸들러
 export const handleCreateReview = async (req, res) => {
-    try {
-        const storeId = parseInt(req.params.storeId);
-        const reviewData = bodyToReview(req.body);
-        const id = await createReview(storeId, reviewData);
-        res.status(StatusCodes.CREATED).json({ id });
-    } catch (err) {
-        res.status(StatusCodes.BAD_REQUEST).json({ message: err.message });
-    }
+  try {
+    const storeId = parseInt(req.params.storeId);
+    const reviewData = bodyToReview(req.body);
+    const id = await createReview(storeId, reviewData);
+    res.status(StatusCodes.CREATED).json({ id });
+  } catch (err) {
+    res.status(StatusCodes.BAD_REQUEST).json({ message: err.message });
+  }
 };
 
 // 특정 가게에 등록된 리뷰 목록을 조회하는 핸들러
 export const handleListStoreReviews = async (req, res, next) => {
-    /*
-  #swagger.summary = '상점 리뷰 목록 조회 API';
-  #swagger.responses[200] = {
-    description: "상점 리뷰 목록 조회 성공 응답",
-    content: {
-      "application/json": {
-        schema: {
-          type: "object",
-          properties: {
-            resultType: { type: "string", example: "SUCCESS" },
-            error: { type: "object", nullable: true, example: null },
-            success: {
-              type: "object",
-              properties: {
-                data: {
-                  type: "array",
-                  items: {
-                    type: "object",
-                    properties: {
-                      id: { type: "number" },
-                      store: { type: "object", properties: { id: { type: "number" }, name: { type: "string" } } },
-                      user: { type: "object", properties: { id: { type: "number" }, email: { type: "string" }, name: { type: "string" } } },
-                      content: { type: "string" }
-                    }
+  /*
+#swagger.summary = '상점 리뷰 목록 조회 API';
+#swagger.responses[200] = {
+  description: "상점 리뷰 목록 조회 성공 응답",
+  content: {
+    "application/json": {
+      schema: {
+        type: "object",
+        properties: {
+          resultType: { type: "string", example: "SUCCESS" },
+          error: { type: "object", nullable: true, example: null },
+          success: {
+            type: "object",
+            properties: {
+              data: {
+                type: "array",
+                items: {
+                  type: "object",
+                  properties: {
+                    id: { type: "number" },
+                    store: { type: "object", properties: { id: { type: "number" }, name: { type: "string" } } },
+                    user: { type: "object", properties: { id: { type: "number" }, email: { type: "string" }, name: { type: "string" } } },
+                    content: { type: "string" }
                   }
-                },
-                pagination: { type: "object", properties: { cursor: { type: "number", nullable: true } }}
-              }
+                }
+              },
+              pagination: { type: "object", properties: { cursor: { type: "number", nullable: true } }}
             }
           }
         }
       }
     }
-  };
+  }
+};
 */
-    res.status(StatusCodes.OK).success(reviews);
+  res.status(StatusCodes.OK).success(reviews);
 };
 
 // 특정 사용자가 작성한 리뷰 목록을 조회하는 핸들러
 export const handleListUserReviews = async (req, res, next) => {
-    try {
-        const userId = parseInt(req.params.userId);
-        // 유효성 확인
-        if (isNaN(userId)) {
-            throw new InvalidUserIdFormatError();
+  try {
+    /*
+#swagger.summary = '사용자가 작성한 리뷰 목록 API';
+#swagger.responses[200] = {
+  description: "사용자 리뷰 목록 조회 성공 응답",
+  content: {
+    "application/json": {
+      schema: {
+        type: "object",
+        properties: {
+          resultType: { type: "string", example: "SUCCESS" },
+          error: { type: "object", nullable: true, example: null },
+          success: {
+            type: "object",
+            properties: {
+              data: {
+                type: "array",
+                items: {
+                  type: "object",
+                  properties: {
+                    id: { type: "number" },
+                    content: { type: "string"},
+                    score: { type: "number"},
+                    store: { type: "object", properties: { id: { type: "number" }, name: { type: "string" } } },
+                  }
+                }
+              },
+              pagination: { type: "object", properties: { cursor: { type: "number", nullable: true } }}
+            }
+          }
         }
-        const cursor = typeof req.query.cursor === "string" ? parseInt(req.query.cursor) : 0;
-
-        const reviews = await listUserReviews(userId, cursor);
-        res.status(StatusCodes.OK).success(reviews);
-    } catch (err) {
-        next(err);
+      }
     }
+  }
+};
+#swagger.responses[404] = {
+      description: "존재하지 않는 사용자의 리뷰 조회 실패 응답",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              resultType: { type: "string", example: "FAIL" },
+              error: {
+                type: "object",
+                properties: {
+                  errorCode: { type: "string", example: "U002" },
+                  reason: { type: "string" },
+                  data: { type: "object" }
+                }
+              },
+              success: { type: "object", nullable: true, example: null }
+            }
+          }
+        }
+      }
+    };
+    #swagger.responses[400] = {
+      description: "유효하지 않는 userID의 리뷰 조회 실패 응답",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              resultType: { type: "string", example: "FAIL" },
+              error: {
+                type: "object",
+                properties: {
+                  errorCode: { type: "string", example: "U003" },
+                  reason: { type: "string" },
+                  data: { type: "object" }
+                }
+              },
+              success: { type: "object", nullable: true, example: null }
+            }
+          }
+        }
+      }
+    };
+*/
+    res.status(StatusCodes.OK).success(reviews);
+  } catch (err) {
+    next(err);
+  }
 };
