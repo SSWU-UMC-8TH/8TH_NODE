ae2d31eda83b599d97e78aad36122eabbeaacd0b
taeyoung0524 <miseoseo1@naver.com>
2025-05-23 17:59:49 +0900

    사용자가 작성한 리뷰 보여주기 응답 - 성공 Swagger 주석 추가

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -17,11 +17,46 @@ export const handleAddReview = async (req, res, next) => {
 };
 
 export const handleShowMyReview = async (req, res, next) => {
+    /*
+    #swagger.summary = "사용자가 작성한 리뷰 보여주기",
+    #swagger.responses[200] = {
+        description: "사용자가 작성한 리뷰 보여주기 성공 응답",
+        content:{
+            "application/json" :{
+                schema: {
+                    type:"object", 
+                    properties: {
+                        resultType:{type:"string", example:"SUCCESS"},
+                        error:{type:"string", nullable:true, example:null},
+                        success: {
+                            type:"object",
+                            properties:{
+                                data:{
+                                    type:"array", 
+                                    items: {
+                                        type:"object", 
+                                        properties: {
+                                            id: {type:"number", example:1},
+                                            review:{type:"string", example:"자리가 많고 조용해서 좋아요~"},
+                                            score:{type:"string"},
+                                            storeName:{type:"string", example:"스타벅스 강남역점"},
+                                            storeAddress: {type:"string", example:"강남구 강남대로 123"}
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+    */
     try{
         const userId = parseInt(req.params.userId);
         const reviews = await showMyReview(userId);
 
-        res.status(StatusCodes.OK).success(reviews);
+        res.status(StatusCodes.OK).success({data: reviews});
     } catch(error){
         next(error);
     }
