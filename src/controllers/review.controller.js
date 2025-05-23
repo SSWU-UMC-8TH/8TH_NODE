0ce420a57275ddca0cbe74581aae02f6ebe55cf3
taeyoung0524 <miseoseo1@naver.com>
2025-05-24 00:55:38 +0900

    가게에 리뷰 추가하기 API -> Swagger 주석 추가

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -4,6 +4,75 @@ import { createReviewService } from "../services/review.service.js";
 import { showMyReview } from "../services/review.service.js";
 
 export const handleAddReview = async (req, res, next) => {
+    /*
+    #swagger.summary = "가게에 리뷰 추가하기 요청",
+    #swagger.requestBody = {
+        required:true,
+        content:{
+            "application/json": {
+                schema : {
+                    type:"object",
+                    properties: {
+                        userId:{type:"number", example:1},
+                        review:{type:"string", example:"자몽허니블랙티 너무 맛있어요~"},
+                        score:{type:"number", example:5},
+                        storeId:{type:"number",example:2}
+                    }
+                }
+            }
+        }
+    };
+    #swagger.responses[200] = {
+        description:"가게에 리뷰 추가하기 성공 응답",
+        content:{
+            "application/json":{
+                schema: {
+                    type:"object", 
+                    properties:{
+                        resultType:{type:"string", nullable:true, example:"SUCCESS"},
+                        error: {type:"object", nullable: true, example:null},
+                        success: {
+                            type:"object", 
+                            properties: {
+                                userId:{type:"number"},
+                                review:{type:"string"},
+                                score:{type:"number"},
+                                storeId:{type:"number"}
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+    #swagger.responses[400] = {
+        description :"가게에 리뷰 추가하기 실패 응답",
+        content: {
+            "application/json" : {
+                schema : {
+                    type:"object", 
+                    properties: {
+                        resultType: {type:"string", example:"FAIL"},
+                        error :{
+                            type:"object",
+                            properties: {
+                                errorCode:{type:"string", example:"U001"},
+                                reason : {type:"string"},
+                                data:{type:"object"}
+                            }
+                        },
+                        success: {
+                            type:"object", 
+                            nullable:true, 
+                            example:null
+                        }
+                    }
+                }   
+            }
+        }
+    };
+    */
     try {
         const storeId = parseInt(req.params.storeId);
         const reviewData = bodyToReview(req.body, storeId);
