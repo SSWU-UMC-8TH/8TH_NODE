85f61f2df1de4a8813a2f2ceeb152010cf0570c4
chaeyeon089 <chaeyeon0091@naver.com>
2025-06-02 02:38:52 +0900

    9주차 실습

--- src/index.js
+++ src/index.js
@@ -9,7 +9,19 @@ import { getUserReviewsController, createReview } from "./controllers/review.con
 import { handleMissionCreate } from "./controllers/mission.controller.js";
 import { handleChallengeMission } from "./controllers/challenge_mission.controller.js";
 import { getStoreMissions, getUserInProgressMissions } from './controllers/mission.controller.js';
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import session from "express-session";
+import passport from "passport";
+import { googleStrategy } from "./auth.config.js";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 dotenv.config();
+
+passport.use(googleStrategy);
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
 
 const app = express();
 const port = process.env.PORT;
@@ -34,6 +46,25 @@ app.use(cors()); // cors 방식 허용
 app.use(express.static("public")); // 정적 파일 접근
 app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000, // ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(
   "/docs",
   swaggerUiExpress.serve,
@@ -66,6 +97,7 @@ app.get("/openapi.json", async (req, res, next) => {
 });
 
 app.get("/", (req, res) => {
+  console.log(req.user);
   res.send("Hello World!");
 });
 
@@ -85,6 +117,16 @@ app.get('/api/v1/users/:userId/missions/in-progress', getUserInProgressMissions)
 
 // 내가 작성한 리뷰 목록 조회 API
 app.get("/api/v1/users/:userId/reviews", getUserReviewsController);
+app.get("/oauth2/login/google", passport.authenticate("google"));
+
+app.get(
+  "/oauth2/callback/google",
+  passport.authenticate("google", {
+    failureRedirect: "/oauth2/login/google",
+    failureMessage: true,
+  }),
+  (req, res) => res.redirect("/")
+);
 
 app.use((err, req, res, next) => {
   if (res.headersSent) {
